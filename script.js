48d0cb5da0fda8ee27757c248c7b9a7d2731ace3
Reya <150582472+Reya0707@users.noreply.github.com>
2024-01-04 20:54:25 +0000

    moved script into script.js file

---
+++ script.js
@@ -0,0 +1,10 @@
+  
+      function support() {
+        let name = prompt("What is your name? 🐶");
+        alert("Hello 👋" + name + ", Thank you for showing an interest in the most amazing dog breed🐾💙!")
+      }
+
+      let supportHemp = document.querySelector("button");
+      supportHemp.addEventListener("click", support);
+
+    
